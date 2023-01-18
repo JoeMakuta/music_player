cdc46c78311eddef3b8adede44dac8a3835ee5f5
Josué Makuta <makutajosue@gmail.com>
2023-01-18 21:01:37 +0100

    Feat : play local

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const audio = document.getElementById("audio");
-const playButton = document.querySelector(".play");
-const pauseButton = document.querySelector(".pause");
+const playButton = document.querySelector(".play_button");
 const music = "Jenn_Johnson,_You're_Gonna_Be_ok_(Lyric_Video)(128k)";
+const play_icon = document.getElementById("play_icon");
 
 const playSong = () => {
   audio.play();
+  playButton.classList.add("playing");
+  playButton.classList.remove("paused");
+  play_icon.setAttribute("name", "pause");
 };
 
 const pauseSong = () => {
   audio.pause();
+  playButton.classList.remove("playing");
+  playButton.classList.add("paused");
+  play_icon.setAttribute("name", "play");
 };
 
 const loadSong = (song) => {
   audio.src = `music/${song}.mp3`;
-  audio.controls = false
+  audio.controls = false;
 };
 
 playButton.addEventListener("click", () => {
-  playSong();
+  if (audio.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
 });
 
-pauseButton.addEventListener("click", () => {
-   pauseSong();
- });
+// pauseButton.addEventListener("click", () => {
+//    pauseSong();
+//  });
 
 loadSong(music);
 console.log(audio);
